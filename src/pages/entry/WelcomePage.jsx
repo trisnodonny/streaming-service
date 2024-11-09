6ef82f24a033a0e6e148a7c6ab42d320378081f7
trisnodonny <trisnodonny@gmail.com>
2024-11-09 10:33:47 +0700

    update welcome page

--- src/pages/entry/WelcomePage.jsx
+++ src/pages/entry/WelcomePage.jsx
@@ -2,7 +2,12 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useFetchData } from "@services/useFetchData";
 import Footer from "@components/entry/Footer";
-import welcomeImage from '@assets/welcome-image.jpg';
+import welcomeImage from "@assets/images/welcome-image.jpg";
+import welcomeImage2 from "@assets/images/welcome-image2.jpg";
+import exclusive from "@assets/icons/exclusive.png";
+import noAds from "@assets/icons/no-ads.png";
+import offlineViewing from "@assets/icons/offline-viewing.png";
+import personalize from "@assets/icons/personalize.png";
 
 export default function WelcomePage() {
   const options = {
@@ -16,21 +21,43 @@ export default function WelcomePage() {
   const [url, setUrl] = useState("https://api.themoviedb.org/3/movie/popular");
   const { data, isPending, erorr } = useFetchData(url, options);
   const [reasons, setReasons] = useState([
-    "Exclusive Content",
-    "Personalized Recommendations",
-    "Ad-Free Experience",
-    "Offline Viewing",
+    {
+      id: 1,
+      name: "Exclusive Content",
+      icon: exclusive,
+    },
+    {
+      id: 2,
+      name: "Personalized Recommendations",
+      icon: personalize,
+    },
+    {
+      id: 3,
+      name: "Ad-Free Experience",
+      icon: noAds,
+    },
+    {
+      id: 4,
+      name: "Offline Viewing",
+      icon: offlineViewing,
+    },
   ]);
 
   return (
     <>
       <div className="relative bg-gradient-to-b from-zinc-700 to-black min-h-screen">
-        <img className="absolute opacity-10 z-0" src={welcomeImage} alt="welcome" />
+        <div className="w-full h-full">
+          <img
+            className="absolute opacity-40 z-0 bg-blue-500 h-max w-full"
+            src={welcomeImage2}
+            alt="welcome"
+          />
+        </div>
         <header className="container mx-auto max-w-[1400px] text-white relative z-10">
           <div className="flex items-center justify-between py-6 px-4">
             <div>logo</div>
             <Link
-              className="flex items-center font-medium py-1 px-4 rounded-md bg-blue-500 hover:bg-blue-600 transition-all"
+              className="flex items-center font-medium py-1 px-4 rounded-md bg-sky-700 hover:bg-sky-800 transition-all"
               to="/login"
             >
               Sign In
@@ -40,25 +67,32 @@ export default function WelcomePage() {
 
         <main className="container mx-auto max-w-[1200px] p-8 text-white sm:px-6 md:px-8 relative z-10">
           <div className="w-full bg-blue-200 h-[450px] mx-auto mb-4 sm:mb-8"></div>
-          <div className="max-w-[720px] mx-auto px-4 mb-4 sm:mb-8">
+          <div className="max-w-[768px] mx-auto px-4 mb-4 sm:mb-8">
             <p className="text-center text-2xl sm:text-4xl font-bold">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Stream the latest movies, hit TV shows, and exclusive content—on
+              any device, at any time.
             </p>
           </div>
 
           <div className="w-full mx-auto mb-4 sm:mb-8">
             <p className="text-xl md:text-3xl font-bold mb-2 sm:mb-4">
-              Lorem ipsum dolor sit.
+              Reasons to join us
             </p>
             <div className="w-full flex items-stretch flex-wrap justify-between gap-y-2">
-              {reasons.map((reason, index) => (
+              {reasons.map((reason) => (
                 <div
-                  key={index}
-                  className="flex items-center p-4 rounded-[1rem] bg-zinc-800 border border-zinc-700 lg:flex-col lg:items-start lg:gap-8 lg:justify-between w-full lg:w-[calc(25%-8px)]"
+                  key={reason.id}
+                  className="flex items-center p-4 rounded-[1rem] bg-zinc-800 border border-zinc-700 lg:flex-col lg:items-start lg:gap-4 lg:justify-between w-full lg:w-[calc(25%-8px)]"
                 >
-                  <p className="flex w-full">{reason}</p>
+                  <p className="flex w-full">{reason.name}</p>
                   <div className="flex justify-end w-full">
-                    <div className="w-[50px] h-[50px] lg:w-[25px] lg:h-[25px] bg-white"></div>
+                    <div className="w-[50px] h-[50px] bg-sky-700 p-2 rounded-full">
+                      <img
+                        className="invert"
+                        src={reason.icon}
+                        alt={reason.name}
+                      />
+                    </div>
                   </div>
                 </div>
               ))}
@@ -67,12 +101,14 @@ export default function WelcomePage() {
 
           <div className="w-full mx-auto">
             <div className="text-center mb-2 sm:mb-4">
-              <h1 className="font-bold text-5xl mb-2 sm:mb-4">
-                Lorem ipsum dolor sit.
+              <h1 className="text-xl sm:text-4xl font-bold mb-2 sm:mb-8">
+                Discover all this and more, only here.
               </h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe,
-                consequatur!
+              <p className="text-sm sm:text-xl mb-4 sm:mb-8">
+                Myflix is the ultimate destination for a wide variety of
+                content, featuring original series, blockbuster movies,
+                documentaries, and shows for every age group, from drama to
+                comedy to thrillers.
               </p>
             </div>
             <ul className="w-full flex item-stretch flex-wrap justify-between gap-y-4">
