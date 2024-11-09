f6ab3e9dab15418d4c9b50644a6646544209d46c
trisnodonny <trisnodonny@gmail.com>
2024-11-09 08:34:24 +0700

    welcome page update

--- src/pages/entry/WelcomePage.jsx
+++ src/pages/entry/WelcomePage.jsx
@@ -1,50 +1,101 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useFetchData } from "../../services/useFetchData";
+import Footer from "../../components/entry/Footer";
 
 export default function WelcomePage() {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMzAwMzc1Y2Q5MDEwZGJlMzVkZjE4MDI3YzExYTM3OSIsIm5iZiI6MTczMTExMTUyMC43NTAyNTY1LCJzdWIiOiI2NmUxNmIyYzFiYjEzNDlmZWY0MGE0N2QiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.tH1U0p70QV3OVwuLzmy_L2uT720Y7CDcx7TxrICfEh4",
+    },
+  };
+  const [url, setUrl] = useState("https://api.themoviedb.org/3/movie/popular");
+  const { data, isPending, erorr } = useFetchData(url, options);
+
+  console.log(data);
+
   const [reasons, setReasons] = useState([
     "Exclusive Content",
     "Personalized Recommendations",
     "Ad-Free Experience",
     "Offline Viewing",
   ]);
+
   return (
     <>
-      <div className="relative bg-gradient-to-b from-gray-700 to-black min-h-screen">
-        <header className="container mx-auto max-w-[1200px] text-white">
-          <div className="flex items-center justify-between p-4">
+      <div className="relative bg-gradient-to-b from-zinc-700 to-black min-h-screen">
+        <header className="container mx-auto max-w-[1400px] text-white">
+          <div className="flex items-center justify-between py-6 px-4">
             <div>logo</div>
             <Link
               className="flex items-center font-medium py-1 px-4 rounded-md bg-blue-500 hover:bg-blue-600 transition-all"
-              to={"/login"}
+              to="/login"
             >
               Sign In
             </Link>
           </div>
         </header>
 
-        <div className="container mx-auto p-8 text-white sm:px-6 md:px-8">
-          <div className="bg-blue-200 h-[450px] max-w-[968px] mx-auto mb-4 sm:mb-8"></div>
-          <div className="max-w-[720px] mx-auto px-4">
-            <p className="text-center text-2xl sm:text-4xl font-bold mb-4">
+        <main className="container mx-auto max-w-[1200px] p-8 text-white sm:px-6 md:px-8">
+          <div className="w-full bg-blue-200 h-[450px] mx-auto mb-4 sm:mb-8"></div>
+          <div className="max-w-[720px] mx-auto px-4 mb-4 sm:mb-8">
+            <p className="text-center text-2xl sm:text-4xl font-bold">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </p>
           </div>
-          <div className="max-w-[968px] mx-auto">
-            <p className="text-3xl font-bold mb-6">Lorem ipsum dolor sit.</p>
-            <div className="flex items-center w-full flex-wrap justify-between">
+
+          <div className="w-full mx-auto mb-4 sm:mb-8">
+            <p className="text-xl md:text-3xl font-bold mb-2 sm:mb-4">
+              Lorem ipsum dolor sit.
+            </p>
+            <div className="w-full flex items-stretch flex-wrap justify-between gap-y-2">
               {reasons.map((reason, index) => (
                 <div
                   key={index}
-                  className="w-full lg:w-[calc(25%-8px)] p-4 rounded-xl border border-gray-200"
+                  className="flex items-center p-4 rounded-[1rem] bg-zinc-800 border border-zinc-700 lg:flex-col lg:items-start lg:gap-8 lg:justify-between w-full lg:w-[calc(25%-8px)]"
                 >
-                  <p>{reason}</p>
-                  <div className="w-[25px] h-[25px] bg-white mt-4"></div>
+                  <p className="flex w-full">{reason}</p>
+                  <div className="flex justify-end w-full">
+                    <div className="w-[50px] h-[50px] lg:w-[25px] lg:h-[25px] bg-white"></div>
+                  </div>
                 </div>
               ))}
             </div>
           </div>
-        </div>
+
+          <div className="w-full mx-auto">
+            <div className="text-center mb-2 sm:mb-4">
+              <h1 className="font-bold text-5xl mb-2 sm:mb-4">
+                Lorem ipsum dolor sit.
+              </h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe,
+                consequatur!
+              </p>
+            </div>
+            <div className="w-full flex item-stretch flex-wrap justify-between gap-y-4">
+              {data?.results?.slice(0, 12).map((movie, index) => (
+                <Link
+                  key={index}
+                  to={"/login"}
+                  className="w-[calc((100%/2)-10px)] sm:w-[calc((100%/4)-10px)] lg:w-[calc((100%/6)-10px)] rounded-lg overflow-hidden hover:scale-105 transition-all"
+                >
+                  <img
+                    className="w-full"
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    alt={movie.title}
+                  />
+                </Link>
+              ))}
+            </div>
+          </div>
+        </main>
+        <footer className="text-white">
+          <Footer />
+        </footer>
       </div>
     </>
   );
---
+++ src/services/useFetchData.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
+
+export const useFetchData = (url, options = {}) => {
+  const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsPending(true)
+      try {
+        const response = await axios.get(url, options)
+        const data = response.data
+        setIsPending(false)
+        setData(data)
+        setError(null)
+      } catch (err) {
+        setError(`${err}`)
+        setIsPending(false)
+      }
+    }
+    fetchData()
+  }, [url])
+
+  return { data, isPending, error}
+}
