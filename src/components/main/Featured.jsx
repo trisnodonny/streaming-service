facdea664d52840097c96e6b58d480f94a712994
trisnodonny <trisnodonny@gmail.com>
2024-11-11 16:28:32 +0700

    update

--- src/components/main/Featured.jsx
+++ src/components/main/Featured.jsx
@@ -17,10 +17,10 @@ export default function Featured({ url }) {
   return (
     <>
       {data?.results && (
-        <div className="relative" key={randomMovie.id}>
+        <div className="relative" key={randomMovie?.id}>
           <div className="absolute bottom-12 p-4 sm:p-12">
-            <h1 className="text-4xl font-black">{randomMovie.name}</h1>
-            <p className="line-clamp-4">{randomMovie.overview}</p>
+            <h1 className="text-4xl font-black">{randomMovie?.name}</h1>
+            <p className="line-clamp-4">{randomMovie?.overview}</p>
             <div>
               <button className="py-2 px-4 bg-sky-700">Play</button>
               <button className="py-2 px-4 border border-white">Detail</button>
@@ -30,13 +30,13 @@ export default function Featured({ url }) {
             <div
               className="bg-cover bg-center w-full h-lvh blcok sm:hidden"
               style={{
-                backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 1), transparent), url(https://image.tmdb.org/t/p/w500/${randomMovie.poster_path})`,
+                backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 1), transparent), url(https://image.tmdb.org/t/p/w500/${randomMovie?.poster_path})`,
               }}
             ></div>
             <div
               className="bg-cover bg-center w-full h-lvh hidden sm:block"
               style={{
-                backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 1), transparent), url(https://image.tmdb.org/t/p/w500/${randomMovie.backdrop_path})`,
+                backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 1), transparent), url(https://image.tmdb.org/t/p/w500/${randomMovie?.backdrop_path})`,
               }}
             ></div>
           </div>
