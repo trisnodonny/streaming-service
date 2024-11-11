4d89e8d4c6b1e0efeee3f779b7338f1d54101519
trisnodonny <trisnodonny@gmail.com>
2024-11-11 16:25:11 +0700

    update

--- src/components/main/Featured.jsx
+++ src/components/main/Featured.jsx
@@ -11,27 +11,37 @@ export default function Featured({ url }) {
     },
   };
   const { data, pending, error } = useFetchData(url, options);
+  const randomIndex = Math.floor(Math.random() * data?.results?.length);
+  const randomMovie = data?.results[randomIndex];
 
   return (
     <>
-      {data?.results?.slice(0, 1).map((movie) => (
-        <div className="relative" key={movie.id}>
-          <div className="absolute bottom-0">
-            <h1>{movie.name}</h1>
-            <p>{movie.overview}</p>
+      {data?.results && (
+        <div className="relative" key={randomMovie.id}>
+          <div className="absolute bottom-12 p-4 sm:p-12">
+            <h1 className="text-4xl font-black">{randomMovie.name}</h1>
+            <p className="line-clamp-4">{randomMovie.overview}</p>
             <div>
-              <button>Play</button>
-              <button>Detail</button>
+              <button className="py-2 px-4 bg-sky-700">Play</button>
+              <button className="py-2 px-4 border border-white">Detail</button>
             </div>
           </div>
-          <div
-            className="bg-cover bg-center w-full h-lvh"
-            style={{
-              backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 1), transparent), url(https://image.tmdb.org/t/p/w500/${movie.poster_path})`,
-            }}
-          ></div>
+          <div>
+            <div
+              className="bg-cover bg-center w-full h-lvh blcok sm:hidden"
+              style={{
+                backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 1), transparent), url(https://image.tmdb.org/t/p/w500/${randomMovie.poster_path})`,
+              }}
+            ></div>
+            <div
+              className="bg-cover bg-center w-full h-lvh hidden sm:block"
+              style={{
+                backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 1), transparent), url(https://image.tmdb.org/t/p/w500/${randomMovie.backdrop_path})`,
+              }}
+            ></div>
+          </div>
         </div>
-      ))}
+      )}
     </>
   );
 }
