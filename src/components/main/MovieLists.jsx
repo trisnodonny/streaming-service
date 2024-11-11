76e7e179ffe79ba7a11776bf483fbedca8efd36c
trisnodonny <trisnodonny@gmail.com>
2024-11-11 20:32:44 +0700

    update hide api key

--- src/components/main/MovieLists.jsx
+++ src/components/main/MovieLists.jsx
@@ -9,9 +9,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 export default function MovieLists({ label, url }) {
   const navigate = useNavigate();
-  const apiKey =
-    import.meta.env.VITE_API_KEY ||
-    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMzAwMzc1Y2Q5MDEwZGJlMzVkZjE4MDI3YzExYTM3OSIsIm5iZiI6MTczMTExMTUyMC43NTAyNTY1LCJzdWIiOiI2NmUxNmIyYzFiYjEzNDlmZWY0MGE0N2QiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.tH1U0p70QV3OVwuLzmy_L2uT720Y7CDcx7TxrICfEh4";
+  const apiKey = import.meta.env.VITE_API_KEY || "";
   const options = {
     method: "GET",
     headers: {
